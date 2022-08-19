ac75a1e1a303c2ae5d694ba166105403ea07d25f
upsiflu <upsiflu@gmail.com>
2022-08-19 14:21:51 +0200

    Scale video on resize

--- vimeo-video.js
+++ vimeo-video.js
@@ -8,22 +8,25 @@ customElements.define(
     }
     connectedCallback() {
       /**/
-      var vimeoVideo = this;
 
       var vVideo = "714390804?h=3f386f21b9";
       var hVideo = "714389952?h=c32a03d366";
 
-      var clientWidth = document.querySelector("html").clientWidth;
-      var clientHeight = document.querySelector("html").clientHeight;
+      var setSize = () => {
+        var clientWidth = document.querySelector("html").clientWidth;
+        var clientHeight = document.querySelector("html").clientHeight;
+        var lnk = clientHeight > clientWidth ? vVideo : hVideo;
 
-      var lnk = clientHeight > clientWidth ? vVideo : hVideo;
-
-      var htmlt2 = `<iframe  id="vi-banner-video"
+        var htmlt2 = `<iframe  id="vi-banner-video"
        src="https://player.vimeo.com/video/${lnk}&autoplay=0&portrait=0&loop=1&color=70f0a0&title=0&byline=0&dnt=1" 
         width="${clientWidth}" height="${clientHeight}" frameborder="0" 
         allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>`;
 
-      this.innerHTML = htmlt2;
+        this.innerHTML = htmlt2;
+      };
+
+      setSize();
+      window.visualViewport.addEventListener("resize", setSize);
     }
     static get observedAttributes() {
       return [];
